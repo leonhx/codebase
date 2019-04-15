688becf1deb9db1199fe6f54db216a82edc1c774
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:23:59 +0800

    the little schemer: add functions

--- little_schemer/common.rkt
+++ little_schemer/common.rkt
@@ -43,9 +43,49 @@
 (define o+
   (lambda (n m)
     (cond [(zero? m) n]
-          [else (add1 (o+ n (sub1 m)))])))
+          [else (o+ (add1 n) (sub1 m))])))
 
 (define o-
   (lambda (n m)
     (cond [(zero? m) n]
-          [else (sub1 (o- n (sub1 m)))])))
+          [else (o- (sub1 n) (sub1 m))])))
+
+(define addtup
+  (lambda (tup)
+    (cond [(null? tup) 0]
+          [else (o+ (car tup) (addtup (cdr tup)))])))
+
+(define x
+  (lambda (n m)
+    (cond [(zero? m) 0]
+          [else (o+ n (x n (sub1 m)))])))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond [(null? tup1) tup2]
+          [(null? tup2) tup1]
+          [else (cons (o+ (car tup1) (car tup2))
+                      (tup+ (cdr tup1) (cdr tup2)))])))
+
+(define >
+  (lambda (n m)
+    (cond [(zero? n) #f]
+          [(zero? m) #t]
+          (else (> (sub1 n) (sub1 m))))))
+
+(define <
+  (lambda (n m)
+    (cond [(zero? m) #f]
+          [(zero? n) #t]
+          (else (< (sub1 n) (sub1 m))))))
+
+(define =
+  (lambda (n m)
+    (cond [(> n m) #f]
+          [(< n m) #f]
+          (else #t))))
+
+(define expt
+  (lambda (n m)
+    (cond [(zero? m) 1]
+          [else (x n (expt n (sub1 m)))])))
