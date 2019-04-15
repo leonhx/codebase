b4c52cb0c163a1eefc5cd9bfc83da17074a59b25
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:21:22 +0800

    UVaOJ: 1979: new solution

--- POJ/1979.cc
+++ POJ/1979.cc
@@ -1,13 +1,31 @@
-#include <algorithm>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <functional>
+#include <cstdio>
 
-using namespace std;
+char ROOM[21][21];
+int DP[][2] = { { -1, 0 }, { 0, 1 }, { 0, -1 }, { 1, 0 } };
+int X, Y;
+int ch;
+
+int next_char() {
+    while (ch = getchar() && ch == '\n')
+        ;
+    return ch;
+}
 
 int main(int argc, char const *argv[])
 {
+    int W, H;
+    scanf("%d %d\n", &W, &H);
+
+    while (W != 0 && H != 0) {
+        int i = 0, j = 0;
+        while (ch = next_char()) {
+            ROOM[i][j] = ch;
+            i += (j + 1) / W;
+            j += (j + 1) % W;
+        }
+
+        scanf("%d %d\n", &W, &H);
+    }
 
     return 0;
 }
