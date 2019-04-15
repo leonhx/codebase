91a30a6bb8ff9dbdfad4cd3e2ee89c98c6c7b6b1
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:23:58 +0800

    the little schemer: add functions

--- little_schemer/common.rkt
+++ little_schemer/common.rkt
@@ -39,3 +39,13 @@
 
 (define sub1
   (lambda (n) (- n 1)))
+
+(define o+
+  (lambda (n m)
+    (cond [(zero? m) n]
+          [else (add1 (o+ n (sub1 m)))])))
+
+(define o-
+  (lambda (n m)
+    (cond [(zero? m) n]
+          [else (sub1 (o- n (sub1 m)))])))
