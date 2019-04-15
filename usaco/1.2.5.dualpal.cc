d3cd452ceb859277129099ed42f53e15edaae85d
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:20:44 +0800

    1.2.5 dualpal: start

---
+++ usaco/1.2.5.dualpal.cc
@@ -0,0 +1,18 @@
+/*
+ID: xerxes.1
+PROG: dualpal
+LANG: C++11
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream fin ("dualpal.in");
+    ofstream fout ("dualpal.out");
+
+    return 0;
+}
+
