ffde75e12843e705fcd9e8d138391bcc3ff5d334
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:20:47 +0800

    1.3.2 barn1: start

---
+++ usaco/1.3.2.barn1.cc
@@ -0,0 +1,18 @@
+/*
+ID: xerxes.1
+PROG: barn1
+LANG: C++11
+*/
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ifstream fin ("barn1.in");
+    ofstream fout ("barn1.out");
+
+    return 0;
+}
