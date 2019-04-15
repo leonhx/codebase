194cd7c035fc0df0ac259581985af37769c7b6d5
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:20:41 +0800

    1.2.4 palsquare: start

---
+++ usaco/1.2.4.palsquare.cc
@@ -0,0 +1,18 @@
+/*
+ID: xerxes.1
+PROG: palsquare
+LANG: C++11
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream fin ("palsquare.in");
+    ofstream fout ("palsquare.out");
+
+    return 0;
+}
+
