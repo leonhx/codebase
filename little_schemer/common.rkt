93947944410dff2588ef896523b621f6e731a178
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:23:57 +0800

    the little schemer: remove trailing whitespaces

--- little_schemer/common.rkt
+++ little_schemer/common.rkt
@@ -30,6 +30,6 @@
 (define insertR
   (lambda (new old lat)
     (cond [(null? lat) '()]
-          [(eq? old (car lat)) 
+          [(eq? old (car lat))
            (cons (car lat) (cons new (cdr lat)))]
           [else (cons (car lat) (insertR new old (cdr lat)))])))
