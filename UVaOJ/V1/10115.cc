aa1741c2eadf61156314e04136e847cdcc6bf0a3
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:21:22 +0800

    10115: AC. IO problem fixed

--- UVaOJ/V1/10115.cc
+++ UVaOJ/V1/10115.cc
@@ -6,16 +6,19 @@
 
 using namespace std;
 
-bool replace(string& str, const string& from, const string& to) {
+bool replace(string &str, const string &from, const string &to)
+{
     size_t start_pos = str.find(from);
-    if(start_pos == string::npos)
+    if (start_pos == string::npos)
         return false;
     str.replace(start_pos, from.length(), to);
     return true;
 }
 
-void apply_rules(string& str, vector<string> rules) {
-    for (auto i = rules.begin(); i != rules.end(); i += 2) {
+void apply_rules(string &str, vector<string> rules)
+{
+    for (auto i = rules.begin(); i != rules.end(); i += 2)
+    {
         string from = *i;
         string to = *(i + 1);
         while (replace(str, from, to))
@@ -27,27 +30,23 @@ int main(int argc, char const *argv[])
 {
     string tmp;
     int i = 0;
-    while (!cin.eof()) {
+    while (!cin.eof())
+    {
         getline(cin, tmp);
-        tmp.pop_back();
         int N = atoi(tmp.c_str());
         if (N == 0)
             break;
         vector<string> rules;
-        while (N-- > 0) {
+        while (N-- > 0)
+        {
             getline(cin, tmp);
-            tmp.pop_back();
             rules.push_back(tmp);
             getline(cin, tmp);
-            tmp.pop_back();
             rules.push_back(tmp);
         }
         getline(cin, tmp);
-        tmp.pop_back();
         apply_rules(tmp, rules);
-        if (i != 0)
-            cout << endl;
-        cout << tmp;
+        cout << tmp << endl;
         i++;
     }
 
