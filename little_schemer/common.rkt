4cdd4cf0ff0e839f1c52c5927eecfc9c68287676
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:23:56 +0800

    the little schemer: add functions

--- little_schemer/common.rkt
+++ little_schemer/common.rkt
@@ -15,3 +15,14 @@
     (cond [(null? lat) #f]
           [(eq? a (car lat)) #t]
           [else (member? a (cdr lat))])))
+
+(define rember
+  (lambda (a lat)
+    (cond [(null? lat) '()]
+          [(eq? a (car lat)) (cdr lat)]
+          [else (cons (car lat) (rember a (cdr lat)))])))
+
+(define firsts
+  (lambda (l)
+    (cond [(null? l) '()]
+          [else (cons (car (car l)) (firsts (cdr l)))])))
