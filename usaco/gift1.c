e07213fbd7c02ef6b3dcf64002c78e230f5d91dc
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:24:20 +0800

    gift1: reading

---
+++ usaco/gift1.c
@@ -0,0 +1,14 @@
+/*
+ID: xerxes.1
+LANG: C
+TASK: gift1
+*/
+
+#include <stdio.h>
+
+int main() {
+    FILE *fin  = fopen("gift1.in", "r");
+    FILE *fout = fopen("gift1.out", "w");
+
+    return 0;
+}
