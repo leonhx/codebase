c1836139e7d49890c16d8f0b4fe1be488cc2a906
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:20:39 +0800

    1.2.3 namenum: WA

--- usaco/1.2.3.namenum.cc
+++ usaco/1.2.3.namenum.cc
@@ -6,15 +6,51 @@ LANG: C++11
 
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <map>
 
 using namespace std;
 
 const string NONE("NONE");
+const string KEYPAD("ABCDEFGHIJKLMNOPRSTUVWXY");
+
+int str2key(string s) {
+    int i = 0;
+    for (char& c : s) {
+        i *= 10;
+        size_t found = KEYPAD.find(c);
+        if (found != string::npos) {
+            i += found / 3 + 2;
+        } else {
+            return 0;
+        }
+    }
+    return i;
+}
 
 int main() {
     ifstream fin ("namenum.in");
     ifstream fdict ("dict.txt");
     ofstream fout ("namenum.out");
+
+    map<int, string> dict;
+    string t;
+    while (!fdict.eof()) {
+        fdict >> t;
+        int key = str2key(t);
+        if (key != 0)
+            dict[key] = t;
+    }
+
+    int key;
+    fin >> key;
+
+    auto it = dict.find(key);
+    if (it == dict.end()) {
+        fout << NONE << endl;
+    } else {
+        fout << it->second << endl;
+    }
 
     return 0;
 }
