c637e687522492b7764be34f899e223f9a9e908e
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:20:45 +0800

    1.3.1 milk: start

---
+++ usaco/1.3.1.milk.cc
@@ -0,0 +1,15 @@
+/*
+ID: xerxes.1
+PROG: milk
+LANG: C++11
+*/
+
+#include <iostream>
+#include <fstream>
+
+int main() {
+    ifstream fin ("milk.in");
+    ofstream fout ("milk.out");
+
+    return 0;
+}
