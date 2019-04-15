60973149eab381abdd301974512188cb4fc51e36
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:20:48 +0800

    1.3.3 calfflac: start

---
+++ usaco/1.3.3.calfflac.cc
@@ -0,0 +1,18 @@
+/*
+ID: xerxes.1
+PROG: calfflac
+LANG: C++11
+*/
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ifstream fin ("calfflac.in");
+    ofstream fout ("calfflac.out");
+
+    return 0;
+}
