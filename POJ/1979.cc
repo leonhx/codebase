c633ff466632383fdb71f7668cb74e441b36d05b
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:21:21 +0800

    POJ: 1979: start

---
+++ POJ/1979.cc
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
