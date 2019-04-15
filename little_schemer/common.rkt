65249d627e55d0e1fd9c2985bf84ac96f2ffe378
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:23:55 +0800

    the little schemer: add two functions

--- little_schemer/common.rkt
+++ little_schemer/common.rkt
@@ -3,3 +3,15 @@
 (define atom? 
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (x)
+    (cond [(null? x) #t]
+          [(atom? (car x)) (lat? (cdr x))]
+          [else #f])))
+
+(define member?
+  (lambda (a lat)
+    (cond [(null? lat) #f]
+          [(eq? a (car lat)) #t]
+          [else (member? a (cdr lat))])))
