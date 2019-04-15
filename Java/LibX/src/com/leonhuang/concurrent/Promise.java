6939b69ef52535fea1fe80b69c07eb767c5f67de
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:22:07 +0800

    Java LibX: format

--- Java/LibX/src/com/leonhuang/concurrent/Promise.java
+++ Java/LibX/src/com/leonhuang/concurrent/Promise.java
@@ -4,6 +4,7 @@ import com.leonhuang.common.Func0WithErr;
 
 public abstract class Promise<T> implements Func0WithErr<T, Exception> {
     public abstract T apply() throws Exception;
+
     public final Future<T> future() {
         return new Future<T>(this);
     }
