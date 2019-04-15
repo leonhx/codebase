691bdaa75521d6393e4ec2f9063ca344efe7f2fc
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:20:38 +0800

    1.2.3 namenum: ready to start

---
+++ usaco/1.2.3.namenum.cc
@@ -0,0 +1,21 @@
+/*
+ID: xerxes.1
+PROG: namenum
+LANG: C++11
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const string NONE("NONE");
+
+int main() {
+    ifstream fin ("namenum.in");
+    ifstream fdict ("dict.txt");
+    ofstream fout ("namenum.out");
+
+    return 0;
+}
+
