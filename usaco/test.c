fb2f3c29b89631afed62926f8e3ee8c4c578ce40
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-04-16 00:24:18 +0800

    1.1.1: test

---
+++ usaco/test.c
@@ -0,0 +1,19 @@
+/*
+ID: your_id_here
+LANG: C
+TASK: test
+*/
+
+#include <stdio.h>
+
+int main() {
+    FILE *fin  = fopen("test.in", "r");
+    FILE *fout = fopen("test.out", "w");
+
+    int a, b;
+
+    fscanf(fin, "%d %d", &a, &b);  /* the two input integers */
+    fprintf(fout, "%d\n", a+b);
+
+    return 0;
+}
